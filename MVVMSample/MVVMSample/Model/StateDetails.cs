5d74b1bbcb60496f70c55459ba03f58cfd6c49b7
Vimal Prabhu <vimal.manohkaran@syncfusion.com>
2020-03-13 16:02:43 +0530

    Sample added

---
+++ MVVMSample/MVVMSample/Model/StateDetails.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MVVMSample
+{
+    public class StateDetails : INotifyPropertyChanged
+    {
+        #region Private variables
+        private string name, capital, largestcity, language;
+        private double population;
+        private string province;
+        #endregion
+        #region public properties
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        public string Capital
+        {
+            get
+            {
+                return capital;
+            }
+            set
+            {
+                capital = value;
+                OnPropertyChanged(nameof(Capital));
+            }
+        }
+        public string LargestCity
+        {
+            get
+            {
+                return largestcity;
+            }
+            set
+            {
+                largestcity = value;
+                OnPropertyChanged(nameof(LargestCity));
+            }
+        }
+        public string Language
+        {
+            get { return language; }
+            set
+            {
+                language = value;
+                OnPropertyChanged(nameof(Language));
+            }
+        }
+        public string Province
+        {
+            get { return province; }
+            set
+            {
+                province = value;
+                OnPropertyChanged(nameof(Province));
+            }
+        }
+        public double Population
+        {
+            get
+            {
+                return population;
+            }
+            set
+            {
+                population = value;
+                OnPropertyChanged(nameof(Population));
+            }
+        }
+        #endregion
+        #region StateDetails ViewModel
+        public StateDetails(string name, string capital, string city, string language, string province, double population)
+        {
+            Name = name;
+            Capital = capital;
+            LargestCity = city;
+            Language = language;
+            Province = province;
+            Population = population;
+        }
+        #endregion
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string str)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(str));
+        }
+        #endregion    }
+    }
---
+++ MVVMSample/MVVMSample/ViewModel/ViewModel.cs
@@ -0,0 +1,105 @@
+﻿using Syncfusion.SfDataGrid.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVMSample
+{
+
+    class ViewModel : INotifyPropertyChanged
+    {
+        #region Private field
+        private Columns column = new Columns();
+        private ObservableCollection<string> StateProvince = new ObservableCollection<string>();
+        private ObservableCollection<StateDetails> states = new ObservableCollection<StateDetails>();
+        #endregion
+        #region Public Properties
+        public Columns SfGridColumns
+        {
+            get
+            {
+                return column;
+            }
+            set
+            {
+                column = value;
+            }
+        }
+        public ObservableCollection<StateDetails> State
+        {
+            get { return states; }
+            set
+            {
+                states = value;
+                OnPropertyChanged(nameof(State));
+            }
+        }
+        public ObservableCollection<string> StatesProvince
+        {
+            get { return StateProvince; }
+            set
+            {
+                StateProvince = value;
+                OnPropertyChanged(nameof(StatesProvince));
+            }
+        }
+        #endregion
+        #region INotifyProperty Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string str)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(str));
+        }
+        #endregion
+        #region ViewModel Constructor
+        public ViewModel()
+        {
+            AddProvince();
+            AddStateDetails();
+            GenerateColumn();
+        }
+        #endregion
+        #region Private Method
+        private void GenerateColumn()
+        {
+            SfGridColumns.Add(new GridTextColumn() { MappingName = "Name" });
+            SfGridColumns.Add(new GridTextColumn() { MappingName = "LargestCity" });
+            SfGridColumns.Add(new GridNumericColumn() { MappingName = "Population" });
+
+        }
+        private void AddProvince()
+        {
+            StatesProvince.Add("South Central");
+            StatesProvince.Add("Eastern");
+            StatesProvince.Add("North Eastern");
+            StatesProvince.Add("Northern");
+            StatesProvince.Add("South Eastern");
+            StatesProvince.Add("Western");
+        }
+
+        public void AddStateDetails()
+        {
+            State.Add(new StateDetails("California", "Sacramento ", "Sacramento", "English", "Western", 49506799));
+            State.Add(new StateDetails("Florida", "Tallahassee", "Tallahassee", "English", "South Eastern", 72147030));
+            State.Add(new StateDetails("California", "Sacramento ", "Sacramento", "English", "Western", 49506799));
+            State.Add(new StateDetails("Florida", "Tallahassee", "Tallahassee", "English", "South Eastern", 72147030));
+            State.Add(new StateDetails("Texas", "Austin", "Austin", "English", "South Central", 61095297));
+            State.Add(new StateDetails("New Jersey", "Trenton", "Trenton", "English", "North Eastern", 6864602));
+            State.Add(new StateDetails("Texas", "Austin", "Austin", "English", "South Central", 61095297));
+            State.Add(new StateDetails("New Jersey", "Trenton", "Trenton", "English", "North Eastern", 6864602));
+            State.Add(new StateDetails("California", "Sacramento ", "Sacramento", "English", "Western", 49506799));
+            State.Add(new StateDetails("Florida", "Tallahassee", "Tallahassee", "English", "South Eastern", 72147030));
+            State.Add(new StateDetails("Texas", "Austin", "Austin", "English", "South Central", 61095297));
+            State.Add(new StateDetails("New Jersey", "Trenton", "Trenton", "English", "North Eastern", 6864602));
+        }
+
+        #endregion
+    }
+}
+
+
